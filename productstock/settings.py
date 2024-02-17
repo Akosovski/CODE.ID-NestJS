c6fc870b97751d5f8cc91a51ec829cccfdb1518b
Muhammad Akhta <akhtacaesar@gmail.com>
2024-02-17 15:40:13 +0700

    Minor Changes for Deployment

--- productstock/settings.py
+++ productstock/settings.py
@@ -86,28 +86,24 @@ MEDIA_ROOT = 'asia-southeast2/'
 UPLOAD_ROOT = 'asia-southeast2/images/'
 MEDIA_URL = 'https://storage.googleapis.com/{}/'.format(GS_BUCKET_NAME)
 
-# MEDIA_ROOT = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # Database for development environments only
-DATABASES = {
-   'default': {
-       'ENGINE': os.environ.get('DB_ENGINE'),
-       'NAME': os.environ.get('DB_NAME'),
-       'USER': os.environ.get('DB_USER'),
-       'PASSWORD': os.environ.get('DB_PASSWORD'),
-       'HOST': os.environ.get('DB_HOST'),
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': os.environ.get('DB_ENGINE'),
+#        'NAME': os.environ.get('DB_NAME'),
+#        'USER': os.environ.get('DB_USER'),
+#        'PASSWORD': os.environ.get('DB_PASSWORD'),
+#        'HOST': os.environ.get('DB_HOST'),
+#    }
+# }
 
 # Database for production environments
-# DATABASES = {
-#     "default": dj_database_url.config(default=os.environ.get('DB_URL'), conn_max_age=1800)
-# }
+DATABASES = {
+    "default": dj_database_url.config(default=os.environ.get('DB_URL'), conn_max_age=1800)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
